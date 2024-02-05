16e4ca2feee2a74746304513e606cae03ac794a5
xMaFey <uhlir2002@seznam.cz>
2024-02-05 16:22:59 +0000

    Solving Q1

--- src/CA3_Question1.java
+++ src/CA3_Question1.java
@@ -3,18 +3,36 @@
  *  Class Group: GD2b
  */
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.Stack;
 import java.util.Scanner;
+import java.util.Collections;
 
 public class CA3_Question1
 {
-    public static void runSimulation()
-    {
 
+    public static void runSimulation(Integer plateNumber)
+    {
+        Stack<Integer> driveway = new Stack<Integer>();
+        Stack<Integer> street = new Stack<Integer>();
+
+        if(plateNumber < 0){
+            driveway.pop();
+            System.out.println("was popped out");
+        }
+
+        if(plateNumber > 0){
+            plateNumber = plateNumber * -1;
+            driveway.push(plateNumber);
+            System.out.println(plateNumber + " was pushed in the driveway");
+        }
     }
 
     public static void main(String[] args) {
-        runSimulation();
+        Scanner number = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter license plate number: ");
+
+        Integer plateNumber = number.nextInt();  // Read user input
+
+        runSimulation(plateNumber);
     }
 }
