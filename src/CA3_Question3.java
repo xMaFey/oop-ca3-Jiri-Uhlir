a21df5e7f6cb509b987ec6398e14a06cddc6323e
xMaFey <uhlir2002@seznam.cz>
2024-02-06 18:32:42 +0000

    Solving Q3

--- src/CA3_Question3.java
+++ src/CA3_Question3.java
@@ -1,4 +1,8 @@
 import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.TreeMap;
+
 /**
  *  Name: Jiri Uhlir
  *  Class Group: GD2b
@@ -6,9 +10,19 @@ import java.io.FileNotFoundException;
 
 public class CA3_Question3
 {
-    public static void readFile(String fileName)
-    {
+    static Map<String, HashSet<Integer>> map = new TreeMap<>();
 
+    public static void readFile(String CA3_Question1)
+    {
+        //Map<String, HashSet<Integer>> map = new TreeMap<>();
+
+        if(map.containsKey(token)){
+            map.get(token).add(lineNumber);
+        } else{
+            HashSet<Integer> set = new HashSet<>();
+            set.add(lineNumber);
+            map.put(token, set);
+        }
     }
 
     public static void main(String[] args) throws FileNotFoundException {
