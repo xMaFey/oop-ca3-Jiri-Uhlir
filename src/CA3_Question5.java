19a75609a2545d6c7c8c0493bb7d2a6a27cbfa22
xMaFey <uhlir2002@seznam.cz>
2024-02-08 13:03:09 +0000

    Q5 Solved

--- src/CA3_Question5.java
+++ src/CA3_Question5.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
 /**
  *  Name:
  *  Class Group:
@@ -5,9 +9,57 @@
 
 public class CA3_Question5
 {
+    public static Queue<String> takeoffQ = new LinkedList<>();
+    public static Queue<String> landingQ = new LinkedList<>();
+
+    public static void takeoff(String flightSymbol){
+        takeoffQ.offer(flightSymbol);
+    }
+
+    public static void land(String flightSymbol){
+        landingQ.offer(flightSymbol);
+    }
+
+    public static void next(){
+        if(!landingQ.isEmpty()){
+            System.out.println("Landing: " + landingQ.poll());
+        }
+        else if(!takeoffQ.isEmpty()){
+            System.out.println("Takeoff: " + takeoffQ.poll());
+        }
+        else{
+            System.out.println("No planes in queue");
+        }
+    }
 
     public static void main(String[] args)
     {
+        Scanner flightReader = new Scanner(System.in);
 
+        while(true){
+            System.out.println("Enter takeoff or land + flight symbol (to add to the queue); next (to do one landing or takeoff) or quit (to quit the app): ");
+            String input = flightReader.nextLine().trim();
+
+            if(input.startsWith("takeoff")){
+                String flightSymbol = input.split(" ")[1];
+                takeoff(flightSymbol);
+            }
+            else if(input.startsWith("land")){
+                String flightSymbol = input.split(" ")[1];
+                land(flightSymbol);
+            }
+            else if(input.equals("next")){
+                next();
+            }
+            else if(input.equals("quit")){
+                break;
+            }
+            else{
+                System.out.println("Invalid command");
+            }
+        }
+
+        flightReader.close();
     }
+
 }
