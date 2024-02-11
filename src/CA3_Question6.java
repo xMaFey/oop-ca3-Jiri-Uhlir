589654f0c76eee077a47efb628b90848b33b1e0b
xMaFey <uhlir2002@seznam.cz>
2024-02-11 14:01:24 +0000

    Q6 Solved

--- src/CA3_Question6.java
+++ src/CA3_Question6.java
@@ -1,4 +1,6 @@
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 /**
  *  Name: Jiri Uhlir
@@ -6,7 +8,6 @@ import java.util.Scanner;
  */
 public class CA3_Question6
 {
-
     /*
     Will repeatedly ask the user to enter the commands in the format
     buy qty price
@@ -18,7 +19,12 @@ public class CA3_Question6
     public static void main(String[] args) {
 
        Scanner in = new Scanner(System.in);
-        String command="";
+       Queue<Block> shares = new LinkedList<>();
+       double totalGain = 0.0;
+
+        System.out.println("Commands: buy quantity price, sell quantity price, quit");
+
+        String command = "";
             do {
             System.out.print(">");
             command = in.next();
@@ -26,15 +32,45 @@ public class CA3_Question6
             {
                 int qty = in.nextInt();
                 double price = in.nextDouble();
-
+                shares.offer(new Block(qty, price));
+                System.out.println("Bought " + qty + " shares at $" + price + " each");
             }
-            else if(command.equals("sell"))
+            else if(command.equalsIgnoreCase("sell"))
             {
                 int qty = in.nextInt();
-                double price = in.nextDouble();
+                double totalPrice = 0.0;
 
+                while(qty > 0 && !shares.isEmpty()){
+                    Block block = shares.peek();
 
+                    if(block.quantity <= qty){
+                        double sellPrice = in.nextDouble();
+                        totalPrice += block.quantity * (sellPrice - block.price);
+                        qty -= block.quantity;
+                        shares.poll();
+                    } else{
+                        double sellPrice = in.nextDouble();
+                        totalPrice += qty * (sellPrice - block.price);
+                        block.quantity -= qty;
+                        qty = 0;
+                    }
+                }
+
+                System.out.println("Sold shares. Gain on this sale: $" + totalPrice);
+                totalGain += totalPrice;
             }
-        }while(!command.equalsIgnoreCase("quit"));
+        } while(!command.equalsIgnoreCase("quit"));
+
+        System.out.println("Total gain: $" + totalGain);
+    }
+}
+
+class Block{
+    int quantity;
+    double price;
+
+    public Block(int quantity, double price){
+        this.quantity = quantity;
+        this.price = price;
     }
 }
